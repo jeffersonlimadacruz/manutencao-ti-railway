a0c2073c9d2740cebea84ca80c95a8f712698833
jeffersonlimadacruz <jeffersonlimadacruz02@gmail.com>
2026-07-16 22:13:41 -0300

    Add Vite configuration

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'client/src'),
+    },
+  },
+  root: 'client',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+});
